71cbc2a660d00eeea7b4f9855e042b743dcdf21e
anott03 <amitav_nott@ryecountryday.org>
2022-04-24 10:31:09 -0400

    starting UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,17 @@ import Image from 'next/image';
 
 const Home: NextPage = () => {
   return (
-    <div className="text-2xl text-red-200">Hello</div>
+    <div className="text-2xl h-screen w-screen flex flex-col p-96">
+      <div className='text-gray-800 w-full h-full flex flex-col'>
+        <p className='text-gray-100'>Enter a URL:</p>
+        <div className='w-full flex flex-row bg-red-800 p-1 justify-between'>
+          <div className='flex-1 bg-green-100'>
+            <input type="text text-gray-800 border-none w-full" />
+          </div>
+          <button className="text-gray-100 flex-none">Go</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
