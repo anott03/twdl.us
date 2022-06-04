02dfa8c7962ffa5d3c54e1848926b14d157f7582
Amitav Nott <amitav_nott@ryecountryday.org>
2022-06-04 13:45:34 -0400

    finishing api cleanup

--- workers/api/src/app.ts
+++ workers/api/src/app.ts
@@ -1,23 +1,16 @@
 import { Router, Sunder } from "sunder";
-import { customHeader } from "./middleware/customHeader";
 import { renderErrorsAsJSON } from "sunder/middleware/render";
 import { registerRoutes } from "./routes";
-import { renderErrorsAsHTML } from "./middleware/htmlErrors";
 import { Env } from "@/bindings";
-import { handlePreflightRequests, setCorsHeaders } from "./middleware/cors";
+import { setCorsHeaders } from "./middleware/cors";
 
 export function createApp() {
     const app = new Sunder<Env>();
     const router = new Router<Env>();
     registerRoutes(router);
 
-    app.use(customHeader);
     app.use(setCorsHeaders);
-    app.use(handlePreflightRequests);
-    
-    app.use(renderErrorsAsHTML);
     app.use(renderErrorsAsJSON);
-
     app.use(router.middleware);
 
     return app;
--- workers/api/src/middleware/cors.ts
+++ workers/api/src/middleware/cors.ts
@@ -6,14 +6,3 @@ export async function setCorsHeaders(ctx: Context, next: Function) {
     ctx.response.headers.set('Access-Control-Allow-Origin', `*`);
     await next();
 }
-
-export async function handlePreflightRequests(ctx: Context, next: Function) {
-  if (ctx.request.method === 'OPTIONS') {
-    console.log("Preflight Request!")
-    ctx.response.headers.set("Connection", "keep-alive");
-    ctx.response.headers.set("Access-Control-Allow-Origin", "*");
-    ctx.response.headers.set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-    ctx.response.headers.set("Access-Control-Max-Age", "86400");
-  }
-  await next();
-}
