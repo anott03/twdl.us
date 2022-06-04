84c825e19451c72fa8ee10d16504d06bdc211107
Amitav Nott <amitav_nott@ryecountryday.org>
2022-06-04 13:46:30 -0400

    generate url requests finally working

--- client/src/pages/url-shortener/Shortener.tsx
+++ client/src/pages/url-shortener/Shortener.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
 const Shortener: () => JSX.Element = () => {
-  const [ url, setUrl ] = useState("");
-  const [ shortenedUrl, setShortenedUrl ] = useState(null);
+  const [ url, setUrl ] = useState<string>("");
+  const [ shortenedUrl, setShortenedUrl ] = useState<string|null>(null);
 
   const shortenUrl = (e: any) => {
     e.preventDefault();
@@ -21,13 +21,15 @@ const Shortener: () => JSX.Element = () => {
     }
 
     fetch("http://localhost:8787/generate", opts)
-      .then(res => res.json())
-      .then(data => console.log);
+      .then(async (res) => {
+        let json: { id: string } = await res.json();
+        setShortenedUrl(`http://localhost:3000/u/${json.id}`);
+      })
   }
 
   return (
     <div className="w-screen h-screen p-5 bg-slate-800 flex flex-col justify-center items-center">
-      <div className="p-2 border-solid border-[1px] border-white flex flex-row w-full max-w-lg">
+      <div className="p-2 border-solid border-[1px] border-white flex flex-row w-full max-w-xl">
         <input
           className="bg-inherit text-white py-5 px-3"
           style={{flex: 0.95}}
@@ -37,8 +39,8 @@ const Shortener: () => JSX.Element = () => {
         <button className="text-white m-2" onClick={shortenUrl}>Go</button>
       </div>
       { shortenedUrl &&
-        <div className="p-3 m-3 border-solid border-[1px] border-white w-full max-w-lg">
-          <input className="bg-inherit text-white px-3" disabled value={shortenedUrl}/>
+        <div className="p-3 m-3 border-solid border-[1px] border-white w-full max-w-xl">
+          <input className="bg-inherit text-white px-3 w-full" disabled value={shortenedUrl}/>
         </div> }
     </div>
   );
