bf1d1188a7636395b9184f29fc90c7591348ba05
Amitav Nott <amitav_nott@ryecountryday.org>
2022-06-04 11:44:10 -0400

    api worker

---
+++ src/workers/api/src/handlers/generateUrl.ts
@@ -0,0 +1,9 @@
+import { Context } from "sunder";
+import { v4 as uuid } from 'uuid';
+
+export async function generateUrl(ctx: Context) {
+  ctx.response.headers.set("Content-Type", "application/json");
+  ctx.response.body = JSON.stringify({
+    id: uuid()
+  });
+}
