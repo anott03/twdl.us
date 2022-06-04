2c13dffaa4e3ba03e12cbe2170c07de8ab909fd9
Amitav Nott <amitav_nott@ryecountryday.org>
2022-06-03 21:12:29 -0400

    basic template

---
+++ src/pages/landing/Landing.tsx
@@ -0,0 +1,11 @@
+import './Landing.css';
+
+const Landing: () => JSX.Element = () => {
+  return (
+    <div className="w-screen h-screen flex flex-col items-center bg-slate-800 justify-center">
+      <h1 className="text-5xl text-white">Hello</h1>
+    </div>
+  );
+}
+
+export default Landing;
