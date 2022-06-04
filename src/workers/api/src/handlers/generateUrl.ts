471c3367cd3d4f24bc4dea21f1bce67d2f9badc0
Amitav Nott <amitav_nott@ryecountryday.org>
2022-06-04 11:54:08 -0400

    working on api

--- src/workers/api/src/handlers/generateUrl.ts
+++ src/workers/api/src/handlers/generateUrl.ts
@@ -2,6 +2,7 @@ import { Context } from "sunder";
 import { v4 as uuid } from 'uuid';
 
 export async function generateUrl(ctx: Context) {
+  ctx.response.headers.set("Access-Control-Allow-Origin", "*");
   ctx.response.headers.set("Content-Type", "application/json");
   ctx.response.body = JSON.stringify({
     id: uuid()
--- src/workers/api/src/routes.ts
+++ src/workers/api/src/routes.ts
@@ -1,20 +1,9 @@
 import { Router } from "sunder";
 import { Env } from "./bindings";
-import { homeHandler } from "./handlers/home";
 import { serveStaticAssetsFromKV } from "./middleware/static";
 import { generateUrl } from "./handlers/generateUrl";
 
 export function registerRoutes(router: Router<Env>) {
-    router.get("/", homeHandler);
-
-    router.get("/static/:assetPath+", serveStaticAssetsFromKV())
-    router.head("/static/:assetPath+", serveStaticAssetsFromKV())
-
-    // Example inline route with a named parameter
-    router.get("/hello/:name", ({ response, params }) => {
-        response.body = `Hello ${params.name}!`;
-    });
-
     router.get("/robots.txt", (ctx) => {
         // This disallows all bots/scrapers
         ctx.response.body = `Agent: *\nDisallow: /`;
