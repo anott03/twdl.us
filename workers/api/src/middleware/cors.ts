26ea6ee0e3217c41ca0510e5f8e6cc067cc7b851
Amitav Nott <58123848+anott03@users.noreply.github.com>
2022-08-18 00:37:19 -0400

    additional allowed origins

--- workers/api/src/middleware/cors.ts
+++ workers/api/src/middleware/cors.ts
@@ -2,7 +2,7 @@
 import { Context } from "sunder";
 
 export async function setCorsHeaders(ctx: Context, next: Function) {
-    let allowed_origins: string[] = ["https://www.twdl.us"];
+    let allowed_origins: string[] = ["https://www.twdl.us", "https://twdl.us"];
     if (allowed_origins.includes(ctx.request.get('Origin') || "")) {
       ctx.response.headers.set('Content-Type', `application/json`);
       ctx.response.headers.set('Access-Control-Allow-Origin', `*`);
