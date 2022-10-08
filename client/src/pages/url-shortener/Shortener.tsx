2e69499733fb9123f8f5ac9477dcd2d58c0d61ff
Amitav Nott <58123848+anott03@users.noreply.github.com>
2022-10-08 19:57:19 -0400

    removing focus highlight from shortener input

--- client/src/pages/url-shortener/Shortener.tsx
+++ client/src/pages/url-shortener/Shortener.tsx
@@ -31,7 +31,7 @@ const Shortener: () => JSX.Element = () => {
     <div className="w-screen h-screen p-5 bg-slate-800 flex flex-col justify-center items-center">
       <div className="p-2 border-solid border-[1px] border-white flex flex-row w-full max-w-xl">
         <input
-          className="bg-inherit text-white py-5 px-3"
+          className="bg-inherit text-white py-5 px-3 focus:outline-none"
           style={{flex: 0.95}}
           placeholder="enter a url here"
           value={url} onChange={(e: any) => setUrl(e.target.value)}
